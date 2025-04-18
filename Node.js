104bb6df7e506aea85c393eeb7a41de279165471
Akhila Gundekari <76896332+AkhilaGundekari@users.noreply.github.com>
2025-04-17 20:48:17 -0500

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,12 @@
+const db = mysql.createConnection({
+  host: '<your-server>.mysql.database.azure.com',
+  user: 'mysql_admin@<your-server>',
+  password: '<your-password>',
+  database: 'studentdb',
+  ssl: { rejectUnauthorized: true }
+});
+``` azure_CI_CD_documentation 2.docx](file-service://file-MWfaQrYD2XqwuqdWTm7Ddh)  
+"scripts": {
+  "start": "node index.js"
+}
+``` azure_CI_CD_documentation 2.docx](file-service://file-MWfaQrYD2XqwuqdWTm7Ddh)
